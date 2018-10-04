9b3b962501f6e2fb8cee0ff16a5da0368616d618
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-10-04 18:23:16 -0400

    added the git ignore , keys, liri js files

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+var moment = require('moment');
+moment().format();
+
+var request = require()
+var spotify = new Spotify(keys.spotify);
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+}); 
+
+
+
+
+
